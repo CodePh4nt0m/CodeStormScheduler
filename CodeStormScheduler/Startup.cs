25fb92ff043dffcd7612f70739281fb3d7a4e11b
dreamwire <dilnuwan77@gmail.com>
2015-06-07 09:28:21 +0530

    Initial

---
+++ CodeStormScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeStormScheduler.Startup))]
+namespace CodeStormScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
