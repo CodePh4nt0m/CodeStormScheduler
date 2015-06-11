4185ff14d3eb2ed07758c0b4c811ce6a115b9460
dreamwire <dilnuwan77@gmail.com>
2015-06-11 22:59:45 +0530

    facebook login

--- CodeStormScheduler/App_Start/BundleConfig.cs
+++ CodeStormScheduler/App_Start/BundleConfig.cs
@@ -16,27 +16,37 @@ namespace CodeStormScheduler
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/AdminLTE/AdminLTE.min.css",
-                      "~/Content/themes.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/PixelAdmin").Include(
+                      "~/Content/PixelAdmin/bootstrap.min.css",
+                      "~/Content/PixelAdmin/pixel-admin.min.css",
+                      "~/Content/PixelAdmin/widgets.min.css",
+                      "~/Content/PixelAdmin/pages.min.css",
+                      "~/Content/PixelAdmin/rtl.min.css",
+                      "~/Content/PixelAdmin/themes.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/plugincss").Include(
-                      "~/plugins/font-awesome/css/font-awesome.min.css",
-                      "~/plugins/ionicons/css/ionicons.min.css"));
+            bundles.Add(new StyleBundle("~/Content/Custom").Include(
+                      "~/Content/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/pixeladmin").Include(
+                      "~/Scripts/PixelAdmin/bootstrap.min.js",
+                      "~/Scripts/PixelAdmin/pixel-admin.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                      "~/Scripts/PixelAdmin/jquery.slimscroll.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                       "~/Scripts/angular.js",
                       "~/Scripts/angular-route.js",
                       "~/Scripts/angular-cookies.js"));
+
+            
         }
     }
 }
