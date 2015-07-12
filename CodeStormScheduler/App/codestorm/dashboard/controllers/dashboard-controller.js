89e4259b917ca1f4b321ad3b41a2f85ebf80e28d
rootpox <isurusiriwardana@outlook.com>
2015-07-12 20:24:15 +0530

    CodeStorm : Scheduler : dashboard implementation

---
+++ CodeStormScheduler/App/codestorm/dashboard/controllers/dashboard-controller.js
@@ -0,0 +1,8 @@
+﻿dashboardModule.controller('dashboardController', function ($scope) {
+    $scope.message = 'Dashboard controller.';
+    $scope.userName = 'Isuru';
+
+    $scope.map_lat = 6.872753;
+    $scope.map_lng = 79.88367;
+
+});
---
+++ CodeStormScheduler/App/codestorm/dashboard/model/dashboard-meta.js
@@ -0,0 +1,9 @@
+﻿if (mainModule.meta === undefined) {
+    mainModule.meta = {};
+}
+
+var dashboardModule = angular.module('dashboardModule', []);
+
+dashboardModule.config(function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
